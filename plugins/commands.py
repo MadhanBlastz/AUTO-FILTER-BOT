802117e9e926f52db8df20296eb46c9e5dc85a1b
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-07-08 16:13:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -67,10 +67,10 @@ async def start(client, message):
     # Check if the user is interacting for the first time
     if not await db.is_user_exist(message.from_user.id):
     # Add the user to the database
-    await db.add_user(message.from_user.id, message.from_user.first_name)
+     await db.add_user(message.from_user.id, message.from_user.first_name)
     
     # Log the new user interaction
-    await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+     await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     
     # Send welcome message to the new user
     welcome_message = (
@@ -84,7 +84,7 @@ async def start(client, message):
         "You can start by using the available commands or buttons below."
     )
  
-    await client.send_message(message.from_user.id, welcome_message)
+     await client.send_message(message.from_user.id, welcome_message)
 
         # Prepare inline keyboard for individual users
         if PREMIUM_AND_REFERAL_MODE == True:

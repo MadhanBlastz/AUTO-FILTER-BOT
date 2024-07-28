0add5219336fb7a41abc44d8e0c7279d0b003e66
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-07-29 01:00:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -654,7 +654,7 @@ async def start(client, message):
             # Send a new message indicating the file is deleted
             await message.reply_text(
               text="Yᴏᴜʀ ғɪʟᴇ ɪs ᴅᴇʟᴇᴛᴇᴅ ᴛᴏ ᴀᴠᴏɪᴅ ᴄᴏᴘʏʀɪɢʜᴛ©️ ɪssᴜᴇs",
-              reply_markup=InlineKeyboardMarkup(button)
+              reply_markup=InlineKeyboardMarkup(btn)
             )
            
 
@@ -712,8 +712,9 @@ async def start(client, message):
     await msg.delete()
     # Send a new message indicating the file is deleted
     await message.reply_text(
-    text="Yᴏᴜʀ ғɪʟᴇ ɪs ᴅᴇʟᴇᴛᴇᴅ ᴛᴏ ᴀᴠᴏɪᴅ ᴄᴏᴘʏʀɪɢʜᴛ©️ ɪssᴜᴇs"
-     )
+    text="Yᴏᴜʀ ғɪʟᴇ ɪs ᴅᴇʟᴇᴛᴇᴅ ᴛᴏ ᴀᴠᴏɪᴅ ᴄᴏᴘʏʀɪɢʜᴛ©️ ɪssᴜᴇs",
+     
+    reply_markup=InlineKeyboardMarkup(btn))
     await k.edit_text("<b>Your File/Video is successfully deleted!!!\n\nClick below button to get your deleted file 👇</b>",reply_markup=InlineKeyboardMarkup(btn))
     return   
 

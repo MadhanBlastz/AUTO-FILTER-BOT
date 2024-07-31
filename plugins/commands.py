429c97e382dc5c32116f5744a9d450055a51e2eb
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-07-31 23:04:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -16,6 +16,8 @@ from database.connections_mdb import active_connection
 from urllib.parse import quote_plus
 from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
 logger = logging.getLogger(__name__)
+from database1 import *
+from helper1 import *
 
 BATCH_FILES = {}
 join_db = JoinReqs

f8a698921875f18eb2964b7cc45aea07ef0e87ca
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-07-31 17:25:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -464,8 +464,8 @@ async def start(client, message):
                 text="<b>Invalid link or Expired link !</b>",
                 protect_content=True
             )
-         is_valid = await check_token(client, userid, token)
-       if is_valid == True:            
+        is_valid = await check_token(client, userid, token)
+        if is_valid == True:            
             await message.reply_text(
                 text=f"<b>Hey {message.from_user.mention}, You are successfully verified !\nNow you have unlimited access for all files for 24 hours.\n\nNow Try Again ♻️</b>",
                 protect_content=True

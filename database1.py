b7b3fd5a46097c02d9429b61b6bdb1abc0c9f7a0
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-08-01 08:31:36 +0530

    Update database1.py

--- database1.py
+++ database1.py
@@ -1,7 +1,7 @@
 
 import motor.motor_asyncio
-DBURI = environ.get('DATABASE_URI', "DATABASE_URI=mongodb+srv://Madhan:N0password@cluster0.y0vtta6.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DB_URI = environ.get('DBURI', "DATABASE_URI=mongodb+srv://Madhan:N0password@cluster0.y0vtta6.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = environ.get('DB_NAME', "Cluster0")
 
 dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
 database = dbclient[DB_NAME]

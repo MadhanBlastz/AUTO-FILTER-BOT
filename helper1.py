96f7514e34bd4a32c937e4a158e9d5ca8eb083df
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-08-01 08:21:24 +0530

    Update helper1.py

--- helper1.py
+++ helper1.py
@@ -11,7 +11,7 @@ from shortzy import Shortzy
 import requests
 import time
 from datetime import datetime
-from database.database import user_data, db_verify_status, db_update_verify_status
+from database1 import user_data, db_verify_status, db_update_verify_status
 
 #logger = logging.getLogger(__name__)
 #logger.setLevel(logging.INFO)

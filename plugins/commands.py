18911da75199a2849db44f6ef6a5738f07e49861
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-08-08 21:55:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -659,7 +659,7 @@ async def start(client, message):
 
             # Send a new message indicating the file is deleted
             await message.reply_text(
-              text="Yᴏᴜʀ ғɪʟᴇ ɪs ᴅᴇʟᴇᴛᴇᴅ ᴛᴏ ᴀᴠᴏɪᴅ ᴄᴏᴘʏʀɪɢʜᴛ©️ ɪssᴜᴇs \nFɪʟᴇ ɴᴀᴍᴇ :{title}",
+              text="Yᴏᴜʀ ғɪʟᴇ ɪs ᴅᴇʟᴇᴛᴇᴅ ᴛᴏ ᴀᴠᴏɪᴅ ᴄᴏᴘʏʀɪɢʜᴛ©️ ɪssᴜᴇs \n f"<b>Fɪʟᴇ ɴᴀᴍᴇ : <code>{files.file_name}</code> ",
               reply_markup=InlineKeyboardMarkup(btn)
             )
             
@@ -718,7 +718,7 @@ async def start(client, message):
     await msg.delete()
     # Send a new message indicating the file is deleted
     await message.reply_text(
-    text="Yᴏᴜʀ ғɪʟᴇ ɪs ᴅᴇʟᴇᴛᴇᴅ ᴛᴏ ᴀᴠᴏɪᴅ ᴄᴏᴘʏʀɪɢʜᴛ©️ ɪssᴜᴇs\nFɪʟᴇ ɴᴀᴍᴇ :{title}",
+    text="Yᴏᴜʀ ғɪʟᴇ ɪs ᴅᴇʟᴇᴛᴇᴅ ᴛᴏ ᴀᴠᴏɪᴅ ᴄᴏᴘʏʀɪɢʜᴛ©️ ɪssᴜᴇs\n f"<b>Fɪʟᴇ ɴᴀᴍᴇ : <code>{files.file_name}</code> ",
      
     reply_markup=InlineKeyboardMarkup(btn))
     

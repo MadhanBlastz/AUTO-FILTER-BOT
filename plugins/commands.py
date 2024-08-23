7c456f5503a8ac33fe504fa11f571770ff12e50f
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-08-24 00:06:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -314,8 +314,7 @@ async def start(client, message):
                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 Fast Download 🚀", url=download),  # we download Link
                                                             InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)]])  # web stream Link
                     )
-                    await asyncio.sleep(900)
-                    await log_msg.delete()
+                    
 
 
 

69a69f3c2ff9e17ad628a79eef85970d133b91da
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-08-23 23:54:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1954,7 +1954,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 Fast Download 🚀", url=download),  # we download Link
                                                     InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)]])  # web stream Link
             )
-            
+            await asyncio.sleep(600)
+            await log_msg.delete()
             
             
             button = [[

99b066587e626cf000006be9d879cdf2f24f94b5
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-08-26 22:33:26 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -143,7 +143,7 @@ class Database:
     
     
     async def total_users_count(self):
-        count = await self.col.count_documents({})
+        count = await self.users_col.count_documents({})
         return count
     
     async def remove_ban(self, id):
@@ -151,35 +151,35 @@ class Database:
             is_banned=False,
             ban_reason=''
         )
-        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
+        await self.users_col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
     
     async def ban_user(self, user_id, ban_reason="No Reason"):
         ban_status = dict(
             is_banned=True,
             ban_reason=ban_reason
         )
-        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
+        await self.users_col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
 
     async def get_ban_status(self, id):
         default = dict(
             is_banned=False,
             ban_reason=''
         )
-        user = await self.col.find_one({'id':int(id)})
+        user = await self.users_col.find_one({'id':int(id)})
         if not user:
             return default
         return user.get('ban_status', default)
 
     async def get_all_users(self):
-        return self.col.find({})
+        return self.users_col.find({})
     
 
     async def delete_user(self, user_id):
-        await self.col.delete_many({'id': int(user_id)})
+        await self.users_col.delete_many({'id': int(user_id)})
 
 
     async def get_banned(self):
-        users = self.col.find({'ban_status.is_banned': True})
+        users = self.users_col.find({'ban_status.is_banned': True})
         chats = self.grp.find({'chat_status.is_disabled': True})
         b_chats = [chat['id'] async for chat in chats]
         b_users = [user['id'] async for user in users]

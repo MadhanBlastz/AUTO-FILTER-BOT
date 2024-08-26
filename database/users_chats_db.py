2342c6e9656b8c8ac14c3d902642e98782951078
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-08-26 22:43:13 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -180,7 +180,7 @@ class Database:
 
     async def get_banned(self):
         users = self.users_col.find({'ban_status.is_banned': True})
-        chats = self.grp.find({'chat_status.is_disabled': True})
+        chats = self.groups_col.find({'chat_status.is_disabled': True})
         b_chats = [chat['id'] async for chat in chats]
         b_users = [user['id'] async for user in users]
         return b_users, b_chats
@@ -189,11 +189,11 @@ class Database:
 
     async def add_chat(self, chat, title):
         chat = self.new_group(chat, title)
-        await self.grp.insert_one(chat)
+        await self.groups_col.insert_one(chat)
     
 
     async def get_chat(self, chat):
-        chat = await self.grp.find_one({'id':int(chat)})
+        chat = await self.groups_col.find_one({'id':int(chat)})
         return False if not chat else chat.get('chat_status')
     
 
@@ -202,14 +202,14 @@ class Database:
             is_disabled=False,
             reason="",
             )
-        await self.grp.update_one({'id': int(id)}, {'$set': {'chat_status': chat_status}})
+        await self.groups_col.update_one({'id': int(id)}, {'$set': {'chat_status': chat_status}})
         
     async def update_settings(self, id, settings):
-        await self.grp.update_one({'id': int(id)}, {'$set': {'settings': settings}})
+        await self.groups_col.update_one({'id': int(id)}, {'$set': {'settings': settings}})
         
     
     async def get_settings(self, id):
-        chat = await self.grp.find_one({'id':int(id)})
+        chat = await self.groups_col.find_one({'id':int(id)})
         if chat:
             return chat.get('settings', self.default_setgs)
         return self.default_setgs
@@ -220,16 +220,16 @@ class Database:
             is_disabled=True,
             reason=reason,
             )
-        await self.grp.update_one({'id': int(chat)}, {'$set': {'chat_status': chat_status}})
+        await self.groups_col.update_one({'id': int(chat)}, {'$set': {'chat_status': chat_status}})
     
 
     async def total_chat_count(self):
-        count = await self.grp.count_documents({})
+        count = await self.groups_col.count_documents({})
         return count
     
 
     async def get_all_chats(self):
-        return self.grp.find({})
+        return self.groups_col.find({})
 
 
     async def get_db_size(self):
